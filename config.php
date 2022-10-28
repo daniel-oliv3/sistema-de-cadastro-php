11829326291c07f1001045e1b6bfb34afaf7d96d
Daniel Oliveira <danielsapup3@gmail.com>
2022-10-28 10:04:58 -0300

    Update

--- config.php
+++ config.php
@@ -9,12 +9,30 @@ $dbName = "sistema-de-cadastro-db";
 
 $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* //teste de conexão
 if($conexao->connect_errno){
     echo "Erro ao conectar com o bando de dados!";
 }else {
     echo "conexão realizada com sucesso!";
 }
-
+*/
 
 /*
 - http://localhost/sistema-de-cadastro-php/config.php
