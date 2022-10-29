68ac2945149a47adfed26e48e4192d85ebfad8dc
Daniel Oliveira <danielsapup3@gmail.com>
2022-10-28 21:33:37 -0300

    Validation

---
+++ testLogin.php
@@ -0,0 +1,37 @@
+<?php
+
+
+
+//print_r($_REQUEST);
+/* ======= Validação ======= */
+if(isset($_POST['submit']) && !empty($_POST['email']) && !empty($_POST['senha'])){
+    //acessa
+    include_once('config.php');
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    /*print_r('E-mail: ' . $email);
+    print_r('<br>');
+    print_r('Senha: ' . $senha);*/
+
+    $sql = "SELECT * FROM usuarios WHERE email = '$email' and senha = '$senha'";
+
+    $result = $conexao->query($sql);
+
+    /*print_r($sql);
+    print_r($result);*/
+
+    //verificar o numero de linhas(rows)existe no Db
+    if(mysqli_num_rows($result) < 1){
+        print_r('Não existe!');
+    }else {
+        print_r('Existe!');
+    }
+
+}else {
+    //não acessa
+    header('Location: login.php');
+}
+
+
+?>
