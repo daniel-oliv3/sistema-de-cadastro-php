3ac2165a67ca30135c1d9a472c7d646ad6fd4dcc
Daniel Oliveira <danielsapup3@gmail.com>
2022-10-31 18:48:33 -0300

    Delete function

--- delete.php
+++ delete.php
@@ -12,17 +12,8 @@ if(!empty($_GET['id'])){
     $result = $conexao->query($sqlSelect);
 
     if($result->num_rows > 0){			
-        while($user_data = mysqli_fetch_assoc($result)){
-            $nome = $user_data['nome']; 
-            $email = $user_data['email'];
-            $senha = $user_data['senha'];
-            $telefone = $user_data['telefone'];
-            $sexo = $user_data['sexo']; 
-            $data_nascimento = $user_data['data_nascimento'];
-            $endereco = $user_data['endereco'];
-            $cidade = $user_data['cidade'];
-            $estado = $user_data['estado'];
-        }						
+        $sqlDelete = "DELETE FROM usuarios WHERE id=$id";
+        $resultDelete = $conexao->query($sqlDelete);						
     }        
 }
 
