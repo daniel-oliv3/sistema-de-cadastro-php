50eaba5d3cd80426d1d90ca16dec9896f2d25eed
Daniel Oliveira <danielsapup3@gmail.com>
2022-10-31 12:36:02 -0300

    Recendo os dados do banco no form edit

--- edit.php
+++ edit.php
@@ -10,17 +10,24 @@
 
 		$result = $conexao->query($sqlSelect);
 
-		print_r($result);
+		if($result->num_rows > 0){			
+			while($user_data = mysqli_fetch_assoc($result)){
+				$nome = $user_data['nome']; //yes exist
+				$email = $user_data['email'];
+				$senha = $user_data['senha'];
+				$telefone = $user_data['telefone'];
+				$sexo = $user_data['sexo'];
+				$data_nascimento = $user_data['data_nascimento'];
+				$endereco = $user_data['endereco'];
+				$cidade = $user_data['cidade'];
+				$estado = $user_data['estado'];
+			}
+			//print_r($nome);						
+		}else {
+			header('Location: sistema.php'); //not exist
+		}
 
-		$nome = $_POST['nome'];
-		$email = $_POST['email'];
-		$senha = $_POST['senha'];
-		$telefone = $_POST['telefone'];
-		$sexo = $_POST['genero'];
-		$data_nascimento = $_POST['data_nascimento'];
-		$endereco = $_POST['endereco'];
-		$cidade = $_POST['cidade'];
-		$estado = $_POST['estado'];
+		
 
 	
 	}
@@ -46,47 +53,47 @@
 			<legend><b>Editar Formulário</b></legend>
 			<br/><br/>
 			<div class="inputBox">
-				<input type="text" name="nome" id="nome" class="inputUser" required>
+				<input type="text" name="nome" id="nome" class="inputUser" value="<?php echo $nome ?>" required>
 				<label for="nome" class="labelInput">Nome completo</label>
 			</div>
 			<br/>
 			<div class="inputBox">
-				<input type="text" name="email" id="email" class="inputUser" required>
+				<input type="text" name="email" id="email" class="inputUser" value="<?php echo $email ?>" required>
 				<label for="email" class="labelInput">Email</label>
 			</div>
 			<br/>
 			<div class="inputBox">
-				<input type="password" name="senha" id="senha" class="inputUser" required>
+				<input type="password" name="senha" id="senha" class="inputUser" value="<?php echo $senha ?>" required>
 				<label for="senha" class="labelInput">Senha</label>
 			</div>
 			<br/>
 			<div class="inputBox">
-				<input type="tel" name="telefone" id="telefone" class="inputUser" required>
+				<input type="tel" name="telefone" id="telefone" class="inputUser" value="<?php echo $telefone ?>" required>
 				<label for="telefone" class="labelInput">Telefone</label>
 			</div>
 			<p>Sexo:</p>
-			<input type="radio" id="feminino" name="genero" value="feminino" required>
+			<input type="radio" id="feminino" name="sexo" value="feminino" <?php echo ($sexo == 'feminino') ? 'checked' : '' ?> required>
 			<label for="feminino">Feminino</label>
-			<input type="radio" id="masculino" name="genero" value="masculino" required>
+			<input type="radio" id="masculino" name="sexo" value="masculino" <?php echo ($sexo == 'masculino') ? 'checked' : '' ?>  required>
 			<label for="masculino">Masculino</label>
 			<br/><br/>
 			<!--======= Data de Nascimento ======= -->
 			<label for="data_nascimento"><b>Data de Nacimento</b></label>
-			<input type="date" name="data_nascimento" id="data_nascimento" required>				
+			<input type="date" name="data_nascimento" id="data_nascimento" value="<?php echo $data_nascimento ?>" required>				
 			
 			<br/><br/>
 			<div class="inputBox">
-				<input type="text" name="endereco" id="endereco" class="inputUser" required>
+				<input type="text" name="endereco" id="endereco" class="inputUser" value="<?php echo $endereco ?>" required>
 				<label for="endereco" class="labelInput">Endereço</label>
 			</div>
 			<br/>
 			<div class="inputBox">
-				<input type="text" name="cidade" id="cidade" class="inputUser" required>
+				<input type="text" name="cidade" id="cidade" class="inputUser" value="<?php echo $cidade ?>" required>
 				<label for="cidade" class="labelInput">Cidade</label>
 			</div>
 			<br/>			
 			<div class="inputBox">
-				<input type="" name="estado" id="estado" class="inputUser" required>
+				<input type="" name="estado" id="estado" class="inputUser" value="<?php echo $estado ?>" required>
 				<label for="estado" class="labelInput">Estado</label>
 			</div>
 			<br/>
